2563e53298ac63a610a49259cb0abbf7fb01b799
kabyajyoti deka <38033553+dekaNotCoder@users.noreply.github.com>
2026-04-21 12:22:37 +0200

    Update apps.py

--- devfolio/app_devfolio/apps.py
+++ devfolio/app_devfolio/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 print("Hello, World!")
+print("Hello", "how are you?")
+
 class AppDevfolioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_devfolio'
